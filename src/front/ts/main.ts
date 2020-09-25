2c3afc03de63c190d775eeecd9e61d0a03ec1142
Fabian Sarmiento <fsarmiento805@gmail.com>
2020-09-25 19:09:48 -0300

    Actualizacion a la guia de ejercicios en clase

--- src/front/ts/main.ts
+++ src/front/ts/main.ts
@@ -5,6 +5,72 @@
  * Project: DAW - CEIoT - Project Structure
  * Brief: Main frontend file (where the logic is)
 =============================================================================*/
+class Main implements EventListenerObject {
+  
+    myf = new MyFramework;
+    counter: number =0;
+    main():void{
+        console.log("Metodo main")
+
+        //Se crea el array de usuarios
+        let usuarios:Array<User>
+        usuarios = new Array<User>();
+        usuarios.push(new User(1, "Benjamin", "bsarmiento@yahoo.com" ));
+        usuarios.push(new User(2, "Maria", "mvillamil@yahoo.com" ));
+        usuarios.push(new User(3, "Milena", "mviltes@yahoo.com" ));
+
+        this.mostrarUsers(usuarios);
+
+        this.myf = new MyFramework();
+        
+        //myf.configClick("boton", ()=> (this.evento))
+        //myf.configClick("boton", this.evento)
+
+        let b: HTMLElement = this.myf.getElementById("boton")
+        b.addEventListener("click", this)
+        this.myf.configClick ("click", "boton", this);
+        //b.textContent = "Hola mundo!!!"; 
+       // b.addEventListener("click",this.evento)
+
+       this.myf.requestGET("Devices.txt", this);
+    }
+
+    mostrarUsers(users:Array<User>):void{
+       // for (let i in users){
+       //     users[i].printInfo();
+        // }
+        for (let o of users)
+        {
+            o.printInfo();
+        }
+    }
+
+    handleEvent(evt: Event): void {
+        console.log("Se hizo click");
+        console.log(this)  
+        let b:HTMLElement = this.myf.getElementByEvent(evt);
+        console.log(b);
+        this.counter++
+        b.textContent = `click ${this.counter}`
+        //throw new Error("Method not implemented."); 
+    }  
+    
+
+    handleGETResponse(status:number, response:string):void{
+        console.log ("Llego la respuesta del request");
+        }
+    
+
+}
+
+
+
+
+window.onload = ()=> {
+    let m:Main = new Main();
+    m.main();
+}
+
 
 //=======[ Settings, Imports & Data ]==========================================
 
@@ -16,6 +82,9 @@ function greeter(person) {
     return "Hello, " + person;
  }
  
- document.body.innerHTML = greeter(user);
+ //document.body.innerHTML = greeter(user);
+
+ console.log("Hola mundo");
+
 
 //=======[ End of file ]=======================================================
