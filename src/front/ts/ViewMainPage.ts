3e681c7bce954198b4e3573ef23f7fcc230f3c13
Fabian Sarmiento <fsarmiento805@gmail.com>
2020-10-08 19:31:23 -0300

    Se agregó el estado por defecto de las lamparas

--- src/front/ts/ViewMainPage.ts
+++ src/front/ts/ViewMainPage.ts
@@ -10,8 +10,18 @@ class ViewMainPage{
         
         for (let dev of list){
 
+            let image  = "lightbulb.png"
+                if (dev.type=="1"){
+                    image = "window.png"
+                }
+
+            let checked = "";
+                if (dev.state == "1"){
+                    checked = "checked";
+                }
+
             e.innerHTML+= `<li class="collection-item avatar">
-                                <img src="static/images/lightbulb.png" alt="" class="circle">
+                                <img src="static/images/${image}" alt="" class="circle">
                                 <span class="title">${dev.name}</span>
                                 <p>${dev.description} </p>
                                     <!--a href="#" secondary-cntent> </a-->
@@ -20,7 +30,7 @@ class ViewMainPage{
                                         <div class="switch">
                                             <label>
                                                 Off
-                                                <input id = "dev_${dev.id}" type="checkbox">
+                                                <input id = "dev_${dev.id}" type="checkbox" checked>
                                                 <span class="lever"></span>
                                                 On
                                             </label>
